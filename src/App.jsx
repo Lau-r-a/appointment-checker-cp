99fbc43beb7afc30b54a98b6951cf76835b836b7
xenya52 <666nikkineko@gmail.com>
2024-08-08 21:38:18 +0200

    backup commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,45 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [menuContent, setMenuContent] = useState(<></>)
+
+  const createNewDoctorContent = () => {
+    setMenuContent(
+      <div>
+        <p>Doctors name</p>
+        <input></input>
+        <p>Doctors appointment URL page</p>
+        <input></input>
+      </div>
+    )
+    console.log(menuContent)
+  }
+  const manageCurrentDoctorsContent = () => {
+    setMenuContent(
+      <div>
+        <button onClick={createNewDoctorContent}>
+          Create new doctor notification
+        </button>
+      </div>
+    )
+  }
+
   return (
-    <div>
+    <div style={{backgroundColor:"Red"}}>
       <h1>Appointment checker</h1>
       <div>
-        <button>Create new doctor notification</button>
-        <button>Manage current doctors</button>
+        <button 
+        onClick={createNewDoctorContent}>
+          Create new doctor notification
+        </button>
+        <button
+        onClick={manageCurrentDoctorsContent}>
+          Manage current doctors
+        </button>
         <button>Notification Settings</button>
+      </div>
+      <div>
+        {menuContent}
       </div>
     </div>
   )
